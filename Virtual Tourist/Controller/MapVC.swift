3bf67f0b436b18fc4384624efd7ee131172186fc
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-12 18:16:15 +0530

    map last coordinates persisted

--- Virtual Tourist/Controller/MapVC.swift
+++ Virtual Tourist/Controller/MapVC.swift
@@ -11,13 +11,45 @@ import MapKit
 
 class MapVC: UIViewController {
 
+    @IBOutlet var mapView: MKMapView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        if let region = MKCoordinateRegion.load(withKey: "mapregion") {
+            mapView.region = region
+            mapView.delegate = self
+        }
     }
+    
+    deinit {
+          mapView.annotations.forEach{mapView.removeAnnotation($0)}
+          mapView.delegate = nil
+          print("deinit: MapViewController")
+    }
+    
+    
+    
+    
+    
 }
 
 extension MapVC : MKMapViewDelegate {
+    func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+        mapView.region.save(withKey: "mapregion")
+    }
+}
+
+extension MKCoordinateRegion {
+    public  func save(withKey key:String) {
+        let locationData = [center.latitude, center.longitude,
+                            span.latitudeDelta, span.longitudeDelta]
+        UserDefaults.standard.set(locationData, forKey: key)
+    }
     
+    public static func load(withKey key:String) -> MKCoordinateRegion? {
+        guard let region = UserDefaults.standard.object(forKey: key) as? [Double] else { return nil }
+        let center = CLLocationCoordinate2D(latitude: region[0], longitude: region[1])
+        let span = MKCoordinateSpan(latitudeDelta: region[2], longitudeDelta: region[3])
+        return MKCoordinateRegion(center: center, span: span)
+    }
 }
