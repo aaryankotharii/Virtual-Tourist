cd79369db4ad8469e37ef64362e0b1fd3dd0411b
aaryankotharii <aaryan.kothari@gmail.com>
2020-05-15 09:52:52 +0530

    New proj added

--- Virtual Tourist/Virtual Tourist/SceneDelegate.swift
+++ Virtual Tourist/Virtual Tourist/SceneDelegate.swift
@@ -2,7 +2,7 @@
 //  SceneDelegate.swift
 //  Virtual Tourist
 //
-//  Created by Aaryan Kothari on 11/05/20.
+//  Created by Aaryan Kothari on 15/05/20.
 //  Copyright © 2020 Aaryan Kothari. All rights reserved.
 //
 
@@ -12,20 +12,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-    let dataController = DataController()
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         guard let _ = (scene as? UIWindowScene) else { return }
-        
-        dataController.load()
-
-        let navigationController = window?.rootViewController as! UINavigationController
-        let mapVC = navigationController.topViewController as! MapVC
-        mapVC.dataController = dataController
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -56,12 +48,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
 
         // Save changes in the application's managed object context when the application transitions to the background.
-        //(UIApplication.shared.delegate as? AppDelegate)?.saveContext()
-        saveViewContext()
-    }
-    
-    func saveViewContext(){
-        try? dataController.viewContext.save()
+        (UIApplication.shared.delegate as? AppDelegate)?.saveContext()
     }
 
 
